ee38d297e8c3786da844e9d76116914fc630691e
srikanths <savitha.srikanth@gmail.com>
2009-08-05 02:39:30 -0400

    commit11

--- apps/app01/javascripts/Savitha_CallEditNew.js
+++ apps/app01/javascripts/Savitha_CallEditNew.js
@@ -1,5 +1,9 @@
 (function(){
 
+del_footer_button(1);
+add_footer_button(1,'Save','saveAllDetails()','_top',40);
+del_footer_button(2);
+add_footer_button(2,'Save & New Call','saveAndNewAllDetails()','_top',40);
 delete_button('Call Details', 1);
 delete_button('Call Details', 1);
 createNewSaveButton('Call Details',1,'Save','saveAllDetails()','_top',40);
@@ -7,6 +11,47 @@ createNewSaveButton('Call Details',2,'Save & New Call','saveAndNewAllDetails()',
 addSamplesDropSec();
 addProdDeailedSec();
 
+function del_footer_button(position) {
+    var i;
+    var mytables = document.getElementsByTagName("table");
+    for (i = 0; i < mytables.length;i++) {
+        try {
+		    if (mytables[i].getAttribute("class") == "footbar") 
+			{
+			  var mytr = mytables[i].getElementsByTagName("tr");
+			  var newtd = mytr[0].deleteCell(position);
+            }
+        } catch (ex) {
+            // you really don't want to enable this unless at wits end
+            // alert("Exception! " + ex.toString());
+        }
+    }
+    return false;
+}
+
+function add_footer_button(position, button_text, url, target_window, features) {
+    var i;
+    var mytables = document.getElementsByTagName("table");
+    for (i = 0; i < mytables.length; i++) {
+        try {
+            if (mytables[i].getAttribute("class") == "footbar") {
+                 var mytr = mytables[i].getElementsByTagName("tr");				
+                 var newtd = mytr[0].insertCell(position);
+                    newtd.innerHTML = "<div class='buttonTD' " +
+                    "onmouseover='toggleNavButton(this);' " +
+                    "onmouseout='toggleNavButton(this);' " +
+					"onclick='"+url+";' \>" +
+                   // "onclick=\"window.open('" + url + "', '" + target_window + "', '" + features + "');\" >" +
+                    button_text + "</div>";
+                return true;
+            }
+        } catch (ex) {
+            // you really don't want to enable this unless at wits end
+            // alert("Exception! " + ex.toString());
+        }
+    }
+    return false;
+}
 
 function delete_button(section_name, position) {
     var i;
@@ -86,13 +131,6 @@ function addProdDeailedSec(){
 
 })();
 
-var proditems = [];
-//var indicationitems = [];
-//var issuesitems = [];
-
-var prodCount = 0;
-//var indicationCount = 0;
-//var issuesCount = 0;
 
 function addNewRowSampleDrop()
 {
@@ -109,59 +147,37 @@ function addNewRowSampleDrop()
 	row += "<td><input name='CallSampDropNew.Quantity' size='20' type='text' value='' class='inputControl' id='CallSampDropNew.Quantity' /></td>";
 	row += "<td><input type='button' name='delete' value='delete' onclick='jQuery(this).parent().parent().parent().parent().parent().remove()'></input></td>";
 	row += "</tr></table></td></tr>";	
-
 	jQuery("#sampleDrop").append(row);
+	ctrowsamp++; 
+    alert("ROW COUNTER Initial: "+ctrowsamp);
 }
 
 function addNewRowProdDet(){
-i = i+1;
-while(prodFlg==1){
-queryProduct();
-prodFlg=0;
-}
-var row = "<tr width='100%'><td colspan='3'>";
+	var row = "<tr width='100%'><td colspan='3'>";
 	row += "<table>";
 	row += "<tr>";
 	row += "<td><p style='color:red'>Product* </p></td>";
-	row += "<td><select STYLE='width: 130px' id='prodNamePrDet<%=i%>' onchange=onDropDownChange (this);></select></td>";
+	row += "<td><select STYLE='width: 130px' id='prodNamePrDet'><option value='none'></option><option value='Arcoxia 120mg'>Arcoxia 120mg</option><option value='Arcoxia 60mg'>Arcoxia 60mg</option><option value='Arcoxia 90mg'>Arcoxia 90mg</option><option value='Crocin'>Crocin</option><option value='Singulair 10x100mg'>Singulair 10x100mg</option><option value='Singulair 20x40mg'>Singulair 20x40mg</option></select></td>";
 	row += "<td>Priority: </td>";
 	row += "<td><input name=CallProdDetailNew.Priority size='5' tabindex='4' type='text' value='' class=inputControl id='CallProdDetailNew.Priority' /></td>";
 	row += "<td><p style='color:red'>Indication*:</p></td>";
-	row += "<td><select name='CallProdDetailNew.Indication' tabindex='5' onchange=onDropDownChange (this); class=inputControl id='CallProdDetailNew.Indication'></select></td>";
+	row += "<td><select name='CallProdDetailNew.Indication' tabindex='5' onchange=onDropDownChange (this); class=inputControl id='CallProdDetailNew.Indication'><option /><option value='Allergy'>Allergy</option><option value='Asthma'>Asthma</option><option value='Arrhythmia'>Arrhythmia</option><option value='Heart Failure'>Heart Failure</option><option value='Syncope'>Syncope</option><option value='Other'>Other</option></select></td>";
 	row += "<td>Issues:</td>";
-	row += "<td><select name='CallProdDetailNew.Issue' tabindex='6' onchange=onDropDownChange (this); class='inputControl' id='CallProdDetailNew.Issue'></select></td>";
+	row += "<td><select name='CallProdDetailNew.Issue' tabindex='6' onchange=onDropDownChange (this); class='inputControl' id='CallProdDetailNew.Issue'><option /><option value='Side Effects'>Side Effects</option><option value='Efficacy'>Efficacy</option><option value='Cost vs. Generics'>Cost vs. Generics</option><option value='Price'>Price</option></select></td>";
 	row += "<td><input type='button' name='delete' value='delete' onclick='jQuery(this).parent().parent().parent().parent().parent().remove()'></input></td>";
 	row += "</tr></table></td></tr>";	
 	jQuery("#prodDetail").append(row);
-	addCombo();
-}
-
-function addCombo(){
-alert("INSIDE COMBO");
-alert("prodFlg:"+prodFlg);
-for(var j=0;j<prodCount;j++){
-	var prodCombo = document.getElementById("prodNamePrDet<%=i%>");  
-    var prodOption = document.createElement("option"); 
-	
-      	prodOption.text=proditems[j];
-		prodOption.value=proditems[j];
-		try {  
-        prodCombo.add(prodOption, null); //Standard  
-        }catch(error) {  
-        prodCombo.add(prodOption); // IE only  
-        } 
-}
 }
 
 function saveAllDetails()
 {
 	//alert("Inside Save All details");
-	var i;
+	/*var i;
 	var e= $(".buttonTD");
 	for(i=0; i < e.length; i++)
 	{
 		e[i].style.disabled = 'true';
-	}
+	}*/
 
 	createNewCallActivity(function() {
 		loadCallDetailsPage();
@@ -175,11 +191,14 @@ function saveAndNewAllDetails()
 {
 	//alert("Inside Save All details");
 	createNewCallActivity(function() {
-		var pathname = window.location.pathname;
-		alert(pathname);	
+	//	var pathname = window.location.pathname;
+	//	alert(pathname);	
+	newcall();
 	});
-	//createProductDetailed(actId);
-	//loadCallDetailsPage();
+}
+
+function newcall(){
+doNavigate("https://secure-ausomxapa.crmondemand.com/OnDemand/user/ContactCallInsert?OMCR0=AAPA-2WJ5PW&OMTGT=ContactCallInsert&OMTHD=ActivityNewNav&OMCBO=Contact&OMRET0=ContactDetail%3focTitle%3dIDC%2bTest3%26OMTGT%3dContactDetailForm%26OMTHD%3dContactDetailNav%26ocEdit%3dY%26OCTYPE%3d%26ocTitleField%3dFull%2bName%26ContactDetailForm.Id%3dAAPA-2WJ5PW&OCNOEDITTYPE=Y&OCTYPE=")
 }
 
 function createWebSerConn(callback)
@@ -215,24 +234,7 @@ function getListData(type, xmlData) {
 		  var fieldName = jQuery(item).get(0).tagName;
 		  var fieldValue = jQuery(item).text();
 		  obj[fieldName] = fieldValue;
-		  });
-		arr.push(obj);
-	});
-	return arr;    
-}
-
-function getListDataProd(type, xmlData) {
-	var arr = [];
-	var a = 0;
-	jQuery(type, xmlData).each(function(index, item) {
-		var obj = {};
-		jQuery(item).children().each(function(index, item) {
-		  var fieldName = jQuery(item).get(0).tagName;
-		  var fieldValue = jQuery(item).text();
-		  obj[fieldName] = fieldValue;
-		  proditems[a++]=fieldValue;
-		  prodCount = a;
-		  });
+		});
 		arr.push(obj);
 	});
 	return arr;    
@@ -292,11 +294,14 @@ function createNewCallActivity(callback){
 		endTimeMod = '';
 	}
 
-	//alert('subjectValue : ' + subjectValue);
-	if(subjectValue == null || subjectValue == '' || contactPerId == null || contactPerId == ''){
-		alert("Subject is the required field!!");
-		$("<span class='requiredText'>required</span>").insertAfter("#ContactCallInsert.Description");
-		return;
+	var contactId = $("input[id='ContactCallInsert.Contact Full Name']").val();
+	var ownerVal = $("input[id='ContactCallInsert.Assigned To']").val();
+
+	if((subjectValue == null || subjectValue == '') || (startTime == null || startTime == '')
+		|| (endTime == null || endTime == '') || (typeVal == null || typeVal == '') || (contactId == null || contactId == '') 
+		|| (ownerVal == null || ownerVal == ''))
+	{
+		validateSubmit('ContactCallInsert','\/OnDemand\/user\/ContactCallInsert?OMCR0='+contactPerId+'&OMTHD=Save&OMTGT=ContactCallInsert&OMCBO=Contact&OCNOEDITTYPE=Y&OMRET0=ContactDetail%3focTitle%3dIDC%2bTest2%26OMTGT%3dContactDetailForm%26OMTHD%3dContactDetailNav%26ocEdit%3dY%26OCTYPE%3d%26ocTitleField%3dFull%2bName%26ContactDetailForm.Id%3dAAPA-2TQZ7P&OCTYPE=', this);
 	}
 	else{
 		var fields = {
@@ -391,10 +396,14 @@ function createActivityIdUsingWeb(fields, fieldsCont, callback)
 							//alert("items : " + items);
 							activityId = items[0].ActivityId;
 							//alert("activityId : " + activityId);
-							createProductDetailInfo(activityId, function(){
-								callback.call();
+							
+							//createProductDetailInfo(activityId, function(){
+							//	callback.call();
+							midway(activityId, function(){
+							callback.call();
 							});
-							//loadCallDetailsPage();
+							
+							//});
 						}
 					});	
 		}
@@ -405,6 +414,21 @@ function createActivityIdUsingWeb(fields, fieldsCont, callback)
 	});
 
 }
+function midway(activityId, callback)
+{
+if(ctrowsamp!=0){
+alert("ROW COUNTER before calling ProductDetailInfo : "+ctrowsamp); 
+//Currently when the below mentioned function is called the 2nd time the Sample is not created as it is taking 
+//the same row twice because of the similar ID tag in the HTML. 
+createProductDetailInfo(activityId, function(){midway(activityId, function(){callback.call();
+});});
+}
+else 
+{
+callback.call();
+}
+}
+
 
 function createProductDetailInfo(activityId, callback)
 {
@@ -521,7 +545,7 @@ function callWebServToGetProdInfo(fieldsProdet, activityId, reqFrom, callback)
 						},								
 						success: function(xmlData, textStatus) {
 							//alert("successssfullllllll getting the product Info");
-							var items = getListDataProd('Product', xmlData);
+							var items = getListData('Product', xmlData);
 							////alert("items : " + items);
 							var productId = items[0].ProductId;
 							//alert("productId : " + productId);
@@ -631,10 +655,8 @@ function callWebServToCreateProdDet(productId, activityId, callback)
 							//alert("Completed");
 						},								
 						success: function(xmlData, textStatus) {
-						alert("successssfullllllllyy created the Product detailed");
-						//var items = getListData('ProductsDetailed', xmlData);
-						//alert("items : " + items);
-						callback.call();
+							alert("successssfullllllllyy created the Product detailed");
+							callback.call();
 							//loadCallDetailsPage();
 						}
 					});	
@@ -714,6 +736,8 @@ function callWebServToCreateSampDrop(productId, activityId, callback)
 							//alert("Completed");
 						},								
 						success: function(xmlData, textStatus) {
+						    ctrowsamp--;
+							alert("ROW COUNTER DECreasing"+ctrowsamp);
 							alert("successssfullllllllyy created the Sample dropped");
 							callback.call();
 						}
@@ -725,145 +749,3 @@ function callWebServToCreateSampDrop(productId, activityId, callback)
 		//return true;
 	});
 }
-
-function queryProduct()
-	{
-		alert("Getting Product Info");
-		var fields = 
-		{
-		  Name: ''
-		  };
-		createWebServCall(fields);
-	}
-
-	function createWebSerConn(callback)
-	{
-		alert("creating connection with Web services");
-		var userName = 'MERCKTEST_CTE01/pfeil';
-		var password = 'method00';
-		try{
-			jQuery.ajax({
-			   url: '/Services/Integration?command=login',
-			   dataType: 'xml',
-			   beforeSend: function(xhr) {
-				   xhr.setRequestHeader('UserName', userName);
-				   xhr.setRequestHeader('Password', password);               
-			   },
-			   complete: function(xhr, textStatus) {
-					alert("created connection with Web services : " + xhr);
-				    callback.call(this, xhr, textStatus);    
-			   }
-		   });	
-		}
-		catch (e) {
-			alert('Error: ' + e.message);
-		}
-
-	}
-
-	function createWebServCall(fields)
-	{
-		createWebSerConn(function(xhr, textStatus){
-			var soapAction = 'document/urn:crmondemand/ws/product/10/2004:ProductQueryPage';
-			var soapRequestTemplate = '' +
-				'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
-				'   <soapenv:Header/>' +
-				'   <soapenv:Body>' +
-				'      <ProductWS_ProductQueryPage_Input xmlns="urn:crmondemand/ws/product/10/2004">' +
-				'		<PageSize>100</PageSize>' +
-				'         <ListOfProduct>' +
-				'            <Product>' +
-				'		        <%=fields%>' +
-				'	         </Product>' +
-				'         </ListOfProduct>' +
-				'        <StartRowNum>0</StartRowNum>' +
-				'      </ProductWS_ProductQueryPage_Input>' +
-				'   </soapenv:Body>' +
-				'</soapenv:Envelope>';		
-
-			var fieldsXML = '';
-			for (fieldName in fields) {
-				fieldsXML += '<' + fieldName + '>' + fields[fieldName] + '</' + fieldName + '>';
-			}
-		var soapRequest = soapRequestTemplate.replace("<%=fields%>", fieldsXML);	
-		try{
-				jQuery.ajax({
-							url: 'https://secure-ausomxapa.crmondemand.com/Services/Integration',
-							type: 'POST',
-							contentType: 'text/xml',
-							dataType: 'xml',
-							data: soapRequest,
-							beforeSend: function(xhr) {
-								alert("Before sending request for Query : " + xhr);
-								xhr.setRequestHeader('SOAPAction', '"' + soapAction + '"');  
-							},   
-							error: function(errormessage){
-							  alert("error:" +errormessage.responseText);
-							  },
-							complete: function(xhr, textStatus) {
-								alert("Completed");
-							},	                					  
-							success: function(xmlData, textStatus){
-								alert("successssfullllllll " + xmlData);
-								var items = getListDataProducts('Product', xmlData);
-								alert("items : " + items);
-								var productId = items[0].ProductId;
-								//alert("productId : " + productId);
-							}
-						});	
-			}
-			catch (e) {
-				alert('Error: ' + e.message);
-			}
-			//return true;
-		});
-	}
-	
-	function getListDataProducts(type, xmlData) {
-	    var arr = [];
-		var a=0;
-		jQuery(type, xmlData).each(function(index, item) {
-			var obj = {};
-			jQuery(item).children().each(function(index, item) 
-			{
-			  var fieldName = jQuery(item).get(0).tagName;
-			  var fieldValue = jQuery(item).text();
-			  obj[fieldName] = fieldValue;
-			  proditems[a++]=fieldValue;
-			  alert("FieldName:"+fieldName+"FieldValue:"+fieldValue);
-			});
-			arr.push(obj);
-			prodCount = a;
-			});
-		return arr;    
-	}
-
-/*function getListDataIndicationAndIssues(type, xmlData) {
-	    var arr = [];
-		var a = 0;
-		var b = 0;
-		jQuery(type, xmlData).each(function(index, item) {
-			var obj1 = {};
-			var obj2 = {};
-			jQuery(item).children().each(function(index, item) 
-			{
-			  var fieldName1 = jQuery(item).get(0).tagName;
-			  var fieldValue1 = jQuery(item).text();
-			  obj1[fieldName1] = fieldValue1;
-			  indicationitems[a++]=fieldValue1;
-			  alert("FieldName:"+fieldName+"FieldValue:"+fieldValue);
-			  
-			 var fieldName2 = jQuery(item).get(1).tagName;
-			  var fieldValue2 = jQuery(item).text();
-			  obj2[fieldName2] = fieldValue2;
-			  issuesitems[b++] = fieldValue2;
-			  alert(fieldName2+":"+fieldValue2); 
-			 });
-			arr.push(obj);
-			prodCount = a;
-			indicationCount = b;
-			issuesCount = c;
-			
-			});
-		return arr;    
-	} */
