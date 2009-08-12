60279a89ec78c0305cd8dabbd06f6d1a80d81eed
srikanths <savitha.srikanth@gmail.com>
2009-08-12 08:49:45 -0400

    commit

--- common/javascripts/application_loader.js
+++ common/javascripts/application_loader.js
@@ -5,7 +5,7 @@
 
     // URL builder helper
     function appURL(relativePath) {
-      var basePath = 'http://github.com/savithasr/abhinaw/tree/master';
+      var basePath = 'http://github.com/savithasr/abhinaw/raw/master';
 	  alert("Path : " + basePath + relativePath);
       return basePath + relativePath;
     }
